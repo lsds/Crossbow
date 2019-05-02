eead16972562431f2c619db4660744f7e6653d22
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2019-05-02 08:29:56 +0000

    Fix typo

--- src/main/java/uk/ac/imperial/lsds/crossbow/types/UpdateModel.java
+++ src/main/java/uk/ac/imperial/lsds/crossbow/types/UpdateModel.java
@@ -2,7 +2,7 @@ package uk.ac.imperial.lsds.crossbow.types;
 
 public enum UpdateModel {
 	
-	DEFAULT(0), WORKER(1), EAMSGD(2), SYNCHRONOUSEAMSGD(3), DOWNPOUR(4), HOGWILD(5), POLYAK_PUPPERT(6), SMA(7);
+	DEFAULT(0), WORKER(1), EAMSGD(2), SYNCHRONOUSEAMSGD(3), DOWNPOUR(4), HOGWILD(5), POLYAK_RUPPERT(6), SMA(7);
 	
 	private int id;
 	
@@ -22,7 +22,7 @@ public enum UpdateModel {
 		case 3: return "SYNCHRONOUSEAMSGD";
 		case 4: return "DOWNPOUR";
 		case 5: return "HOGWILD";
-		case 6: return "POLYAK-PUPPERT";
+		case 6: return "POLYAK-RUPPERT";
 		case 7: return "SMA";
 		default:
 			throw new IllegalArgumentException ("error: invalid update model type");
@@ -37,7 +37,7 @@ public enum UpdateModel {
 		else if (model.toUpperCase().equals("SYNCHRONOUSEAMSGD")) return SYNCHRONOUSEAMSGD;
 		else if (model.toUpperCase().equals("DOWNPOUR"))          return DOWNPOUR;
 		else if (model.toUpperCase().equals("HOGWILD"))           return HOGWILD;
-		else if (model.toUpperCase().equals("POLYAK-PUPPERT"))    return POLYAK_PUPPERT;
+		else if (model.toUpperCase().equals("POLYAK-RUPPERT"))    return POLYAK_RUPPERT;
 		else if (model.toUpperCase().equals("SMA"))               return SMA;
 		else
 			throw new IllegalArgumentException (String.format("error: invalid update model: %s", model));
