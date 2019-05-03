77af309e3f15ef1bb91a8330a294ae435e00b87e
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2019-05-03 14:35:58 +0000

    Protect random number generator with mutex

--- clib-multigpu/image/yarng.cpp
+++ clib-multigpu/image/yarng.cpp
@@ -2,10 +2,14 @@
 
 #include "../random/generator.hpp"
 
+#include <mutex>
+
 using namespace crossbow;
 
 static CrossbowRandomGenerator *generator = NULL;
 static int initialised = 0;
+
+static std::mutex mtx;
 
 void crossbowYarngInit (unsigned int seed) {
 	if (initialised)
@@ -15,6 +19,10 @@ void crossbowYarngInit (unsigned int seed) {
 
 float crossbowYarngNext (float start, float end) {
 	float value = 0;
+    /* Lock */
+	mtx.lock();
 	generator->randomUniformFill (&value, 1, start, end);
+    /* Unlock */
+	mtx.unlock();
 	return value;
 }
